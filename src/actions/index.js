215fe79f6799645fd117555c537ed4c9f640d4c2
Miz85 <nazim@midokura.com>
2016-04-05 02:00:38 +0200

    weather forecast WIP - lecture 57

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY="85e10214fa344ae390dd3319d1272b22";
+const ROOT_URL=`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},fr`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
